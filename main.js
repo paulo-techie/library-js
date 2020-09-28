c11439706fe0a81495bcb26f5ca425b0ed18e938
Paul Omondi <paulo.techie@gmail.com>
2020-09-28 23:37:51 +0300

    Add js code to accept form input

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 let myLibrary = [];
 
+const addbookForm = document.querySelector('#addbook-form');
+const addbookBtn = document.querySelector('#addbook-btn');
+const submitBtn = document.querySelector('#submit-btn');
+
+addbookForm.style.display = "none";
+
+addbookBtn.addEventListener('click', function() {
+  addbookForm.style.display = "block";
+});
+submitBtn.addEventListener('click', function(e)  {
+  addBookToLibrary;
+  e.preventDefault();
+});
+
 function Book(author, title, pages, readStatus) {
   this.author = author;
   this.title = title;
@@ -8,5 +22,9 @@ function Book(author, title, pages, readStatus) {
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  const author = document.querySelector('#author').value;
+  const title = document.querySelector('#title').value;
+  const pages = document.querySelector('#pages').value;
+  const readStatus = document.querySelector('#readStatus').checked;
+  myLibrary.push(new Book(author, title, pages, readStatus));
 }
