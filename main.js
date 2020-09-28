5e8f57d4bbc9eb3a99b6789bf474783543424139
Paul Omondi <paulo.techie@gmail.com>
2020-09-28 23:08:31 +0300

    create html form to add book

---
+++ main.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(author, title, pages, readStatus) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.readStatus = readStatus;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
